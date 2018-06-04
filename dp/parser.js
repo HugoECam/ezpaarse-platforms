12027c986c76793a2000778a4069c21742ed9965
AGCooper <alexander.g.cooper@gmail.com>
2018-06-04 14:09:29 -0400

    Adds parsing for ld.php and some ids

--- dp/parser.js
+++ dp/parser.js
@@ -39,6 +39,8 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     // https://statisticaldatasets.data-planet.com:443/dataplanet/Datasets.html?id=c592841f-6d8e-4dab-b1e6-fae551d8b9ce&operatingMode=RR
     result.rtype    = 'REF';
     result.mime     = 'HTML';
+    result.title_id = param.id;
+    result.unitid   = param.id;
   } else if (/^\/dataplanet\/datasheet\/Datasheet.jsp$/i.test(path)) {
     // https://statisticaldatasets.data-planet.com:443/dataplanet/datasheet/Datasheet.jsp?viewID=TREND%7COrg0t%7C%7CTimeUnit0%7C%7C%7C%7C%7Cfalse&sid=660ed6712dcdd%24188&param=7fuKsUAw6JIaGcWOIA3l-RqifedMlg0HY_uTQz4p8LO1D7vyDXzQi1AtR85-tjfzivbBl95C7yGtihBBWu16ws2ktUC9dRra3rq6A335S-JEYVQQ1DtiT7SJmR6BaT_fDVs7scW-EULiF6umwv5w9Bbei1E6qQlhlNU8cf1VyP7w27siS9MquJXYTf8JAZiVRYgkuvzRU1WFi7y5IkeQjw%3D%3D
     result.rtype    = 'ARTICLE';
@@ -73,6 +75,12 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     // http://data-planet.libguides.com:80/srch_process_cs.php?q=10.1016%2FS0140-6736(11)60887-8&action=580&search_source_id=0&layout=tab&start=0&group_id=0&guide_id=398600&f_group_id=&f_guide_type_id=&f_guide_owner_id=&f_guide_tag_ids=&f_guide_subject_ids=&sort=_score
     result.rtype    = 'SEARCH';
     result.mime     = 'HTML';
+  } else if (/^\/ld.php$/i.test(path)) {
+    // http://data-planet.libguides.com:80/ld.php?content_id=21684814
+    result.rtype    = 'REF';
+    result.mime     = 'PDF';
+    result.title_id = param.content_id;
+    result.unitid   = param.content_id;
   }
 
   return result;
