fc99de3f5dd0f38d79693433013549828bc147fc
AGCooper <alexander.g.cooper@gmail.com>
2018-06-21 16:05:42 -0400

    Adds additional parsing functions

--- pr/parser.js
+++ pr/parser.js
@@ -14,7 +14,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
   // uncomment this line if you need parameters
-  // let param = parsedUrl.query || {};
+  let param = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
@@ -23,6 +23,17 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     // http://www.pressreader.com:80/Authentication/ConfirmCookies?_=1528393102616
     result.rtype    = 'CONNECTION';
     result.mime     = 'HTML';
+  } else if (/^\/pressdisplay\/PageViewManager.aspx$/i.test(path)) {
+    // http://library.pressdisplay.com:80/pressdisplay/PageViewManager.aspx?action=showpages&issue=12642018061200000000001001&page=2&page2=3&cpage=1&cpage2=1
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'MISC';
+    result.unitid   = param.issue;
+  } else if (/^\/pressdisplay\/pageview.aspx$/i.exec(path)) {
+    // http://library.pressdisplay.com:80/pressdisplay/pageview.aspx?issue=25262018060900000000001001&page=1&articleid=1840083106&previewmode=2
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'MISC';
+    result.title_id = param.articleid;
+    result.unitid   = param.issue;
   }
 
   return result;
