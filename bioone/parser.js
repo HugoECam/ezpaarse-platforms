85ed2518421a016aa392eca398c4b9aa6f5a575c
Lisa Hamlett <lhamlet@emory.edu>
2019-03-26 14:17:30 -0400

    Update BioOne Parser

--- bioone/parser.js
+++ bioone/parser.js
@@ -6,6 +6,11 @@ const Parser = require('../.lib/parser.js');
 module.exports = new Parser(function analyseEC(parsedUrl) {
   let result = {};
   let path   = parsedUrl.pathname;
+  let param  = parsedUrl.query || {};
+
+  // use console.error for debuging
+     console.error(parsedUrl);
+
   let match;
 
   if ((match = /^\/toc\/(([a-zA-Z]+)\/[0-9]+\/[0-9]+)$/i.exec(path)) !== null) {
@@ -98,6 +103,31 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.title_id = match[2].split('-')[0];
     result.doi      = match[1] + '/'+ match[2];
     result.unitid   = match[2];
+
+  } else if ((match = /^\/search$/i.exec(path)) !== null) {
+    // https://bioone.org:443/search?term=potato
+    result.rtype = 'SEARCH';
+    result.mime =  'MISC';
+
+  } else if ((match = /^\/journalArticle\/Download$/i.exec(path)) !== null) {
+    // https://bioone.org:443/journalArticle/Download?fullDOI=10.1600%2F036364419X697840
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'PDF';
+    result.doi      = param.fullDOI
+    result.unitid   = param.fullDOI
+
+  } else if ((match = /^\/journals\/[a-zA-Z-]+\/volume\-[0-9]+\/issue\-[0-9]+$/i.exec(path)) !== null) {
+    result.rtype = 'TOC';
+    result.mime = 'HTML';
+
+// HELP! I don't know how to parse the IDs for this one.
+
+  } else if ((match = /^\/journals\/[a-zA-Z-]+\/volume\-[0-9]+\/issue\-[0-9]+\/(.*)$/i.exec(path)) !== null) {
+    // https://bioone.org:443/journals/Systematic-Botany/volume-44/issue-1/036364419X697840/Herbarium-Practices-and-Ethics-III/10.1600/036364419X697840.full
+    // https://bioone.org:443/journals/the-journal-of-eukaryotic-microbiology/volume-50/issue-1/j.1550-7408.2003.tb00099.x/Novel-Cytoskeletal-Proteins-in-Protists--Introductory-Remarksa-classinternal-link/10.1111/j.1550-7408.2003.tb00099.x.full
+    result.rtype = 'ARTICLE';
+    result.mime  = 'HTML';
+
   }
 
   return result;
