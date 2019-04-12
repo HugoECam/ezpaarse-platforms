587de2589977c8b5088fc7c030c004c50fc49783
Lisa Hamlett <lhamlet@emory.edu>
2019-04-12 17:46:08 -0400

    Makes Changes to JVIR as Requested by Alex and Clare

--- jvir/parser.js
+++ jvir/parser.js
@@ -32,14 +32,16 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     // https://www.jvir.org:443/article/S1051-0443(16)30566-8/addons
     if (match[6] == 'fulltext') {
       result.mime = 'HTML';
+      result.rtype  = 'ARTICLE';
     }
     if (match[6] == 'pdf') {
       result.mime = 'PDF';
+      result.rtype  = 'ARTICLE';
     }
     if (match[6] == 'addons') {
-      result.mime = 'SUPPLEMENT';
+      result.mime = 'HTML';
+      result.rtype = 'SUPPLEMENT';
     }
-    result.rtype  = 'ARTICLE';     
     result.unitid = match[1] + match[2] + match[3] + match[4] + match[5]
   } else if ((match = /^\/action\/([a-z]+)/i.exec(path)) !== null) {
     // https://www.jvir.org:443/action/showFullTextImages?pii=S1051-0443%2816%2930566-8
@@ -56,6 +58,33 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     }
     result.mime = 'HTML'
     result.unitid = param.pii
+    result.rid = param.pii
+  } else if ((match = /^\/content\/([a-z]+)/i.exec(path)) !== null) {
+    // https://www.jvir.org:443/content/cme
+    // https://www.jvir.org:443/content/sir_supplements
+    result.rtype  = 'SEARCH';
+    result.mime   = 'HTML';
+  } else if ((match = /^\/([a-z]+)/i.exec(path)) !== null) {
+    // https://www.jvir.org:443/inpress
+    // https://www.jvir.org:443/issues
+    // https://www.jvir.org:443/multimedia
+    // https://www.jvir.org:443/current
+    if (match[1] == 'inpress') {
+      result.rtype = 'SEARCH';
+      result.mime   = 'HTML';
+    }
+    if (match[1] == 'issues') {
+      result.rtype = 'SEARCH';
+      result.mime   = 'HTML';
+    }
+    if (match[1] == 'multimedia') {
+      result.rtype = 'SEARCH';
+      result.mime   = 'HTML';
+    }
+    if (match[1] == 'current') {
+      result.rtype = 'TOC';
+      result.mime   = 'HTML';
+    }
   } else if ((match = /^\/cms\/([0-9.]+)\/([a-zA-Z0-9.]+)\/([a-z]+)\/([a-zA-Z0-9-]+)\/([a-zA-Z0-9.]+)$/i.exec(path)) !== null) {
     // https://www.jvir.org:443/cms/10.1016/j.jvir.2016.09.028/attachment/58dd870f-e7e3-40e9-afed-1897cd74fe9c/mmc2.mp4
     // https://www.jvir.org:443/cms/10.1016/j.jvir.2016.09.028/attachment/8f4a8bdc-6d6a-4fb2-a962-2d5691cfd0c6/mmc1.mp4
