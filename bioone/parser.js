e3473fdf6211efd59687719255bdc4b735a885a1
AGCooper <alexander.g.cooper@gmail.com>
2019-04-12 13:58:34 -0400

    Updates bioone to roll back bad delete

--- bioone/parser.js
+++ bioone/parser.js
@@ -6,6 +6,11 @@ const Parser = require('../.lib/parser.js');
 module.exports = new Parser(function analyseEC(parsedUrl) {
   let result = {};
   let path   = parsedUrl.pathname;
+  let param  = parsedUrl.query || {};
+
+  // use console.error for debuging
+  // console.error(parsedUrl);
+
   let match;
 
   if ((match = /^\/toc\/(([a-zA-Z]+)\/[0-9]+\/[0-9]+)$/i.exec(path)) !== null) {
@@ -98,8 +103,50 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.title_id = match[2].split('-')[0];
     result.doi      = match[1] + '/'+ match[2];
     result.unitid   = match[2];
+
+  } else if ((match = /^\/search$/i.exec(path)) !== null) {
+    // https://bioone.org:443/search?term=potato
+    result.rtype = 'SEARCH';
+    result.mime =  'MISC';
+
+  } else if ((match = /^\/browse/i.exec(path)) !== null) {
+    // https://bioone.org:443/browse/title/W
+    // https://bioone.org:443/browse/subject/Environmental-Sciences
+    result.rtype = 'SEARCH';
+    result.mime =  'MISC';
+
+  } else if ((match = /^\/journalArticle\/Download$/i.exec(path)) !== null) {
+    // https://bioone.org:443/journalArticle/Download?fullDOI=10.1600%2F036364419X697840
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'PDF';
+    result.doi      = param.fullDOI;
+    result.unitid   = param.fullDOI;
+
+  } else if ((match = /^\/journals\/[a-zA-Z-]+\/volume-[0-9]+\/issue-[0-9]+$/i.exec(path)) !== null) {
+    // https://bioone.org:443/journals/acta-chiropterologica/volume-20/issue-2
+    result.rtype = 'TOC';
+    result.mime = 'HTML';
+
+  } else if ((match = /^\/journals\/[a-zA-Z-]+\/issues$/i.exec(path)) !== null) {
+    // https://bioone.org:443/journals/waterbirds/issues
+    result.rtype = 'TOC';
+    result.mime = 'HTML';
+
+  } else if ((match = /^\/journals\/[a-zA-Z-]+\/issues\/[0-9]+$/i.exec(path)) !== null) {
+    // https://bioone.org:443/journals/waterbirds/issues/2018
+    result.rtype = 'TOC';
+    result.mime = 'HTML';
+
+  } else if ((match = /^\/journals\/[a-zA-Z-]+\/volume-[0-9]+\/issue-[0-9]+\/.*\/([a-zA-Z-]+)\/(([0-9.]+\/(.*))).full$/i.exec(path)) !== null) {
+    // https://bioone.org:443/journals/Systematic-Botany/volume-44/issue-1/036364419X697840/Herbarium-Practices-and-Ethics-III/10.1600/036364419X697840.full
+    // https://bioone.org:443/journals/the-journal-of-eukaryotic-microbiology/volume-50/issue-1/j.1550-7408.2003.tb00099.x/Novel-Cytoskeletal-Proteins-in-Protists--Introductory-Remarksa-classinternal-link/10.1111/j.1550-7408.2003.tb00099.x.full
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'HTML';
+    result.title_id = match[1];
+    result.doi      = match[2];
+    result.unitid   = match[3];
+
   }
 
   return result;
 });
-
