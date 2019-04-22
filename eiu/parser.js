a0a01bbdc8f53f5d3dacd5333d52d25170c4e39c
Lisa Hamlett <lhamlet@emory.edu>
2019-04-22 18:03:53 -0400

    Makes Clare's Changed to EIU

--- eiu/parser.js
+++ eiu/parser.js
@@ -7,6 +7,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
   let param = parsedUrl.query || {};
+  // console.error(parsedUrl);
 
   let match;
 
@@ -16,6 +17,26 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
       result.rtype    = 'SEARCH';
       result.mime     = 'HTML';
     }
+  } else if ((match = /^\/([A-z-]+)$/i.exec(path)) !== null) {
+    // http://country.eiu.com:80/united-kingdom
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+  } else if ((match = /^\/([A-z_-]+)\/([A-z_-]+)$/i.exec(path)) !== null) {
+    // http://www.eiu.com:80/industry/Telecommunications
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+  } else if ((match = /^\/([A-z_-]+)\/([A-z_-]+)\/([A-z_-]+)$/i.exec(path)) !== null) {
+    // http://www.eiu.com:80/industry/financial-services/theme
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+  } else if ((match = /^\/([A-z_-]+)\/([A-z_-]+)\/([A-z_-]+)\/([A-z_-]+)$/i.exec(path)) !== null) {
+    // http://country.eiu.com:80/Ghana/ArticleList/Updates/Politics
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+  } else if ((match = /^\/([A-z_-]+)\/([A-z_-]+)\/([A-z_-]+)\/([0-9]+)\/([A-z_-]+)$/i.exec(path)) !== null) {
+    // http://www.eiu.com:80/industry/telecommunications/company/210029221/telstra
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
   } else if ((match = /^\/industry\/article\/([0-9]+)\/([A-z0-9-]+)\/([0-9-]+)$/i.exec(path)) !== null) {
     // http://www.eiu.com:80/industry/article/2000497984/usa-food-us-potato-giant-bets-on-biotech-potatoes/2013-05-15
     result.rtype    = 'ARTICLE';
@@ -29,11 +50,21 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.title_id = param.article_id;
     result.unitid   = param.article_id;
   } else if ((match = /^\/article.aspx$/i.exec(path)) !== null) {
+    if (param.subtopic == 'Charts and tables') {
+    // http://country.eiu.com:80/article.aspx?articleid=1357877519&Country=Ghana&topic=Economy&subtopic=Charts+and+tables&subsubtopic=Annual+data+and+forecast
+    result.rtype    = 'DATA';
+    result.mime     = 'HTML';
+    result.title_id = param.articleid;
+    result.unitid   = param.articleid;
+    }
+    if (param.subtopic !== 'Charts and tables') {
     // http://country.eiu.com:80/article.aspx?articleid=1987582182
+    // http://country.eiu.com:80/article.aspx?articleid=107592994&Country=Germany&topic=Economy&subtopic=Forecast&subsubtopic=Policy+trends
     result.rtype    = 'ARTICLE';
     result.mime     = 'HTML';
     result.title_id = param.articleid;
     result.unitid   = param.articleid;
+    }
   } else if ((match = /^\/handlers\/filehandler.ashx$/i.exec(path)) !== null) {
     // http://industry.eiu.com:80/handlers/filehandler.ashx?issue_id=777081261&mode=pdf
     result.rtype    = 'ARTICLE';
@@ -46,6 +77,12 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.mime     = 'PDF';
     result.title_id = param.fi;
     result.unitid   = param.fi;
+  } else if ((match = /^\/FileHandler.ashx$/i.exec(path)) !== null) {
+    // http://country.eiu.com:80/FileHandler.ashx?issue_id=1017877485&mode=pdf
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'PDF';
+    result.title_id = param.issue_id;
+    result.unitid   = param.issue_id;
   } else if ((match = /^\/EIUTableView.aspx$/i.exec(path)) !== null) {
     // http://data.eiu.com:80/EIUTableView.aspx?initial=true&pubtype_id=1353181320
     result.rtype    = 'DATA';
