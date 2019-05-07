4d8f2acea921e0153788e7a4bfd434746be987af
clare <grimalkinclare@gmail.com>
2019-05-07 14:04:23 -0400

    adds parser for f1000

---
+++ f1000/parser.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+'use strict';
+const Parser = require('../.lib/parser.js');
+
+/**
+ * Recognizes the accesses to the platform Faculty 1000
+ * @param  {Object} parsedUrl an object representing the URL to analyze
+ *                            main attributes: pathname, query, hostname
+ * @param  {Object} ec        an object representing the EC whose URL is being analyzed
+ * @return {Object} the result
+ */
+module.exports = new Parser(function analyseEC(parsedUrl, ec) {
+  let result = {};
+  let path   = parsedUrl.pathname;
+  // uncomment this line if you need parameters
+  // let param = parsedUrl.query || {};
+
+  // use console.error for debuging
+  // console.error(parsedUrl);
+
+  let match;
+
+  if (/^\/prime\/sections$|browse|search$|topics/i.test(path)) {
+    // https://f1000.com:443/prime/sections?keywords=rainbow+environment+forest&hp=2
+    // https://f1000research.com:443/search?q=rainbow
+    // https://f1000research.com:443/browse/documents?&selectedDomain=documents
+    // https://blog.f1000.com:443/topics/open-data/
+    result.rtype = 'SEARCH';
+    result.mime  = 'HTML';
+  }
+
+
+
+  if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.pdf)$/i.exec(path)) !== null) {
+    // http://parser.skeleton.js/platform/path/to/document-123456-test.pdf?sequence=1
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'PDF';
+    result.title_id = match[1];
+
+    /**
+     * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
+     * it described the most fine-grained of what's being accessed by the user
+     * it can be a DOI, an internal identifier or a part of the accessed URL
+     * more at http://ezpaarse.readthedocs.io/en/master/essential/ec-attributes.html#unitid
+     */
+    result.unitid = match[2];
+
+  } else if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.html)$/i.exec(path)) !== null) {
+    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'HTML';
+    result.title_id = match[1];
+    result.unitid   = match[2];
+  }
+
+  return result;
+});
