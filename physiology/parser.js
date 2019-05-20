e8dfea2e39d60c553b07caddd8c6bf7d39fcda6c
clare <grimalkinclare@gmail.com>
2019-05-20 12:07:13 -0400

    adds contact to manifesto

--- physiology/parser.js
+++ physiology/parser.js
@@ -28,15 +28,15 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.rtype = 'SEARCH';
     result.mime  = 'HTML';
 
-  } else if ((match = /^\/(journal|topic)\/([a-z0-9-\/]+)$/i.exec(path)) !== null) {
+  } else if ((match = /^\/(journal|topic)\/([a-z0-9-/]+)$/i.exec(path)) !== null) {
     // https://www.physiology.org:443/journal/ajpheart
     // https://www.physiology.org:443/topic/advances-collections/2018-institute-for-teaching-and-learning?seriesKey=&tagCode=
     result.rtype = 'TOC';
     result.mime  = 'HTML';
-    result.title_id = match[2]
-    result.unitid = match[2]
+    result.title_id = match[2];
+    result.unitid = match[2];
 
-  } else if ((match = /^\/toc\/([a-z-]+)\/([a-z0-9-\/]+)/i.exec(path)) !== null) {
+  } else if ((match = /^\/toc\/([a-z-]+)\/([a-z0-9-/]+)/i.exec(path)) !== null) {
     // https://www.physiology.org:443/toc/ajprenal/316/5
     // https://www.physiology.org:443/toc/physiologyonline/34/3
     // https://www.physiology.org:443/toc/ajprenal/current
@@ -52,7 +52,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.title_id = match[1];
     result.unitid = match[1];
 
-  } else if ((match = /^\/doi\/abs\/([0-9a-z-\/.]+)$/i.exec(path)) !== null) {
+  } else if ((match = /^\/doi\/abs\/([0-9a-z-/.]+)$/i.exec(path)) !== null) {
     // https://www.physiology.org:443/doi/abs/10.1152/jappl.1967.22.4.782
     result.rtype = 'ABS';
     result.mime = 'HTML';
@@ -60,7 +60,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.unitid = match[1];
     result.doi = match[1];
 
-  } else if ((match = /^\/doi\/full\/([0-9a-z-\/.]+)$/i.exec(path)) !== null) {
+  } else if ((match = /^\/doi\/full\/([0-9a-z-/.]+)$/i.exec(path)) !== null) {
     // https://www.physiology.org:443/doi/full/10.1152/ajpheart.00004.2019
     // https://www.physiology.org:443/doi/full/10.1152/physiol.00007.2019
     // https://www.physiology.org:443/doi/full/10.1152/advan.00045.2019
@@ -70,7 +70,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.unitid = match[1];
     result.doi = match[1];
 
-  } else if ((match = /^\/doi\/pdf\/([0-9a-z-\/.]+)$/i.exec(path)) !== null) {
+  } else if ((match = /^\/doi\/pdf\/([0-9a-z-/.]+)$/i.exec(path)) !== null) {
     // https://www.physiology.org:443/doi/pdf/10.1152/ajpheart.00004.2019
     // https://www.physiology.org:443/doi/pdf/10.1152/physiol.00007.2019
     // https://www.physiology.org:443/doi/pdf/10.1152/jappl.1967.22.4.782
